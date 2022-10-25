74c570ce900580aa60c25dc90c551499e4bf9521
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-10-25 20:42:46 +0530

    Update Pranavambot.py

--- Pranavambot.py
+++ Pranavambot.py
@@ -398,7 +398,8 @@ async def pause(_, message):
     if chat_id in QUEUE:
         try:
             await app.pause_stream(chat_id)
-            
+    except Exception as e:
+        return await m.edit(str(e))        
         
         
 @bot.on_message(filters.command("resume") & filters.group)
@@ -409,7 +410,8 @@ async def resume(_, message):
     if chat_id in QUEUE:
         try:
             await app.resume_stream(chat_id)
-            
+    except Exception as e:
+        return await m.edit(str(e))        
 
 
 @bot.on_message(filters.command("restart"))
